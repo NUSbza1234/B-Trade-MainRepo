aa64008e0655072df9e35d622cd3793ef7f7b247
Hong Wei Tai <e1138406@u.nus.edu>
2024-07-07 02:18:13 +0800

    MongoDB Atlas

--- backend/index.js
+++ backend/index.js
@@ -21,11 +21,12 @@ const alpaca = new Alpaca({
 
 });
 
-mongoose.connect('mongodb://localhost:27017/users');
-
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch(err => console.error('Could not connect to MongoDB Atlas', err));
 const server = require('http').createServer(app);
 const wss = new WebSocket.Server({ server });
-
+    
 let alpacaSocket;
 let currentSymbol = '';
 let isAuthenticated = false;
